9cdc05dfa135e3fc099f8ee1339f12d1e60456cc
Chantel Williams <cewilliams095@gmail.com>
2024-10-01 18:10:20 -0400

    Connect Front end and Back end
    Connect NDVI Calculator with M2M_API.py (downloads data)
    Have scripts except coordinates as inputs
    start working on rectangle_drawn_listener.py (listens for event: rectangle drawn in leaflet in react asp.net core application)

--- M2M_API_Filters.py
+++ M2M_API_Filters.py
@@ -10,9 +10,8 @@ AccessEarthAccessData = AccessEarthAccessData()
 read_coordinates_from_polygon = AccessEarthAccessData.read_coordinates_from_polygon
 
 
-def create_scene_filter(search_area):
-    polygon = read_coordinates_from_polygon(Convert2GeopandasObject(search_area))
-    AccessEarthAccessData.read_coordinates_from_polygon(Convert2GeopandasObject(search_area))
+def create_scene_filter(polygon): #TODO: feed coordinates from user drawn polygon here
+    polygon = [[35.95397342058534, -76.64996823193624], [35.95397342058534, -75.97293004875567], [35.52305406304399, -75.97293004875567], [35.52305406304399, -76.64996823193624], [35.95397342058534, -76.64996823193624]]
     spatialFilter = {'filterType': "mbr",
                      'lowerLeft': {'latitude': polygon[1][1], 'longitude': polygon[1][0]},
                      'upperRight': {'latitude': polygon[3][1], 'longitude': polygon[3][0]}}
@@ -24,18 +23,18 @@ def create_scene_filter(search_area):
 
 
 def create_temporal_filter():
+    # TODO: set start to beginning on one month previous current_month = datetime.now().month
     temporal_filter = {'start': '2024-08-01', 'end': datetime.datetime.now().strftime("%Y-%m-%d")}
     return temporal_filter
 
 
 
 
-def filter_on_centroid(result_extents):
+def filter_on_centroid(result_extents, coordinates):
     """ find output that intersects center of polygon"""
     # https://stackoverflow.com/questions/4355894/how-to-get-center-of-set-of-points-using-python
     # find centroid
-    # parse_results(result_extents)
-    search_polygon = create_scene_filter(r"C:\Users\cewil\Documents\GitHub\NDVICalculator\InputData\Shapefiles\Pocosin_FWS\Pocosin_FWS\Pocosin_FWS_AOI_WGS84.shp")[1]
+    search_polygon = create_scene_filter(coordinates)[1]
     x,y=zip(*search_polygon)
     center=(max(x)+min(x))/2., (max(y)+min(y))/2.
 
